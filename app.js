715be1ae021c04bb15ed3d7b95d2c396dffcad09
javierpelozo <javpelozo@gmail.com>
2016-10-01 14:41:41 -0400

    update

--- app.js
+++ app.js
@@ -20,3 +20,30 @@ mongo.connect("mongodb://localhost:27017/notas", (err, database) => {
     console.log("Escuchando en el puerto 3000");
   });
 });
+
+app.get('/notas', (req, res) => {
+  db.collection('notas').find({}).toArray((err, docs) => {
+    if(err) res.json({ hecho: false, mensaje: "No se pueden recuperar los datos" });
+    res.json({ hecho: true, notas: docs })
+  })
+});
+
+app.post('/notas', (req, res) => {
+  var nota = req.body;
+
+  db.collection('notas').insert(nota, (err, result) => {
+    if(err) res.json({ hecho: false, mensaje: "No se puede agregar a la base de datos" });
+    console.log("Agregado! " + JSON.stringify(result));
+    res.json({ hecho: true, mensaje: "Nueva nota agregada!" });
+  });
+});
+
+app.delete('/notas/:id', (req, res) => {
+  var id = req.params.id;
+
+  db.collection('notas').removeOn({ _id: id }, (err, result) => {
+    if(err) res.json({ hecho: false, mensaje: "No se ha podido eliminar nota" });
+    console.log("Eliminado " + JSON.stringify(result));
+    res.json({ hecho: true, mensaje: "Ha sido eliminado" });
+  });
+});
